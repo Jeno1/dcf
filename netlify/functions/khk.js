9357957148bdc555ef9066c3d3c33d5369d10dfd
Jeno Borsodi <jeno.borsodi@gmail.com>
2023-07-28 12:22:24 +0200

    First KHK function added

---
+++ netlify/functions/khk.js
@@ -0,0 +1,30 @@
+
+require("./huncestdata.js");
+require("./huncitydata.js");
+
+global.reply_template={microservice:{version:"1.3", build:"20230728121313Z", user:"Guest", authorized:true}, answer:{}};
+
+function handleKhkRequest(request, reply) {
+
+    let params = {};
+  
+    let $reply=reply_template;
+    $reply.microservice.user="Guest";
+    $reply.microservice.authorized=true;
+
+    return $reply;
+}  
+
+exports.handler = async function (event, context) {
+
+    var request={};
+    var reply={};
+
+    reply=handleKhkRequest(request, reply);
+
+    return {
+      statusCode: 200,
+      body: $reply,
+    };
+    
+  };
